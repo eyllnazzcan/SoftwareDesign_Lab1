a1704f2f0ccdcfedc80d6e268255a3b099fb7c7c
eyllnazzcan <eylul.naz.ozcan@gmail.com>
2022-11-19 19:17:25 +0200

    CSV file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+string path = "apartment_buildings_2019.csv";
+
+string[] lines = System.IO.File.ReadAllLines(path);
+int counter = 0;
+foreach(string line in lines)
+{
+    counter++;
+}
+
+Console.WriteLine(counter-1);
+
