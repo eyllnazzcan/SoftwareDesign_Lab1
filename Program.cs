8c15dce6ddac0a32076f9b86385ac9ef3b930adc
anabanobremtnz <90685812+anabanobremtnz@users.noreply.github.com>
2022-11-27 21:38:48 +0200

    Commit (new branch)

--- Program.cs
+++ Program.cs
@@ -29,9 +29,41 @@ namespace CSVFile
             Console.WriteLine("Building count by street: " + (counter-1));
         }
 
+        public static void yearBuilt()
+        {
+            string path = "apartment_buildings_2019.csv";
+            var lines = System.IO.File.ReadAllLines(path);
+            int thisYear = 1960, max = 0, min = thisYear;
+            double sum = 0;
+
+            foreach(var line in lines)
+            {    
+                string[] values = line.Split(";"); // Split each value
+                string year = values[10];
+                var isnumber = int.TryParse(year, out thisYear);
+
+                if(isnumber && thisYear != 0) // Makes sure that it is a number different to 0 
+                {
+                    min = Math.Min(min,thisYear);
+                    max = Math.Max(max,thisYear);
+                    sum = sum + thisYear;
+                }
+            }
+
+            double avg = sum / (lines.Length - 1);
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Min construction year: " + min);
+            Console.WriteLine("Max construction year: " + max);
+            Console.WriteLine("Average construction year: " + ((int)avg));
+        }
+
+
+
         public static void Main()
         {
             countByStreet();
+            yearBuilt();
+
         }
 
     }
